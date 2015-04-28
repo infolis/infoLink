4d51ca6b706837df6c3a0b64c593f27449001619
dritze <dominique@informatik.uni-mannheim.de>
2015-04-28 10:51:55 +0200

    Further test, not working yet

--- src/test/java/io/github/infolis/algorithm/FrequencyBootstrappingTest.java
+++ src/test/java/io/github/infolis/algorithm/FrequencyBootstrappingTest.java
@@ -10,16 +10,23 @@ import io.github.infolis.datastore.DataStoreClientFactory;
 import io.github.infolis.datastore.DataStoreStrategy;
 import io.github.infolis.datastore.FileResolver;
 import io.github.infolis.datastore.FileResolverFactory;
+import io.github.infolis.infolink.luceneIndexing.Indexer;
 import io.github.infolis.model.Execution;
 import io.github.infolis.model.InfolisFile;
 import io.github.infolis.model.InfolisPattern;
+import io.github.infolis.model.StudyContext;
+import io.github.infolis.util.InfolisFileUtils;
 import io.github.infolis.util.SerializationUtils;
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,29 +38,43 @@ import org.slf4j.LoggerFactory;
  * @author domi
  */
 public class FrequencyBootstrappingTest {
-    
+
     private final static DataStoreClient client = DataStoreClientFactory.local();
     private final static FileResolver fileResolver = FileResolverFactory.create(DataStoreStrategy.TEMPORARY);
     Logger log = LoggerFactory.getLogger(SearchTermPositionTest.class);
 
     List<String> pattern = new ArrayList<>();
     List<String> files = new ArrayList<>();
-    
-	private final static List<String> testStrings = Arrays.asList(
-			"Please try to find the ALLBUS 2003 short text snippet that I provided to you.",
-			"Please try to find the allbus in this short text snippet that I provided to you.",
-			"Please try to find the .allbus. in this short text snippet that I provided to you.",
-                        "Please try to find the ALLBUS in this short text snippet that I provided to you."
-			);
-        private final static List<String> terms = Arrays.asList("allbus");
-        
+    String indexDir;
+    String testCorpus = "testCorpus";
 
-	private final List<String> testFiles = new ArrayList<>();
+    private final static List<String> testStrings = Arrays.asList(
+            "Please try to find the ALLBUS 2003 short text snippet that I provided to you.",
+            "Please try to find the allbus in this short text snippet that I provided to you.",
+            "Please try to find the .allbus. in this short text snippet that I provided to you.",
+            "Please try to find the ALLBUS in this short text snippet that I provided to you.",
+            "Please try to find the OECD in this short text snippet that I provided to you."
+    );
+    String testDocument1 = testCorpus + File.separator + "testDocument1";
+    String testDocument2 = testCorpus + File.separator + "testDocument2";
+    String testDocument3 = testCorpus + File.separator + "testDocument3";
+    String testDocument4 = testCorpus + File.separator + "testDocument4";
+    String testDocument5 = testCorpus + File.separator + "testDocument5";
 
-	@Before
+    private final static List<String> terms = Arrays.asList("allbus");
+
+    private final List<String> testFiles = new ArrayList<>();
+
+    @Before
     public void createInputFiles() throws IOException {
-		testFiles.clear();
-    	for (String str : testStrings) {
+        testFiles.clear();
+        InfolisFileUtils.writeToFile(new File(testDocument1), "UTF-8", testStrings.get(0), false);
+        InfolisFileUtils.writeToFile(new File(testDocument2), "UTF-8", testStrings.get(1), false);
+        InfolisFileUtils.writeToFile(new File(testDocument3), "UTF-8", testStrings.get(2), false);
+        InfolisFileUtils.writeToFile(new File(testDocument4), "UTF-8", testStrings.get(3), false);
+        InfolisFileUtils.writeToFile(new File(testDocument5), "UTF-8", testStrings.get(4), false);
+        
+        for (String str : testStrings) {
     		String hexMd5 = SerializationUtils.getHexMd5(str);
     		InfolisFile file = new InfolisFile();
     		file.setMd5(hexMd5);
@@ -61,25 +82,52 @@ public class FrequencyBootstrappingTest {
     		IOUtils.write(str, outputStream);
     		client.post(InfolisFile.class, file);
     		testFiles.add(file.getUri());
-    	}       
+    	}
+        
+//        for (String str : testStrings) {
+//            String hexMd5 = SerializationUtils.getHexMd5(str);
+//            InfolisFile file = new InfolisFile();
+//            file.setMd5(hexMd5);
+//            OutputStream outputStream = fileResolver.openOutputStream(file);
+//            IOUtils.write(str, outputStream);
+//            client.post(InfolisFile.class, file);
+//            testFiles.add(file.getUri());
+//        }
+        indexDir = Files.createTempDirectory("infolis-test-").toString();
+        Indexer.main(new String[]{testCorpus, indexDir});
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        FileUtils.deleteDirectory(new File(indexDir));
     }
 
     @Test
     public void testBootstrapping() throws Exception {
-    	
-    	Execution execution = new Execution();   
-    	execution.getTerms().addAll(terms);
-    	execution.setAlgorithm(FrequencyBasedBootstrapping.class);
-    	execution.getInputFiles().addAll(testFiles);
+
+        Execution execution = new Execution();
+        execution.getTerms().addAll(terms);
+        execution.setAlgorithm(FrequencyBasedBootstrapping.class);
+        execution.getInputFiles().addAll(testFiles);
+        //TODO: not very nice
+        execution.setIndexDirectory(indexDir);
+        execution.setFirstOutputFile(Files.createTempFile("infolis-", ".txt").toString());
+        execution.setSearchTerm(terms.get(0));
+        execution.setSearchQuery("\""+terms.get(0)+"\"");
+        execution.setThreshold(0.0);
         
+
         Algorithm algo = new FrequencyBasedBootstrapping();
-    	algo.setDataStoreClient(client);
-    	algo.setFileResolver(fileResolver);
-    	algo.setExecution(execution);
-    	algo.run();        
-    	
-    	System.out.println(execution.getStudyContexts());
-        System.out.println(execution.getPattern());
+        algo.setDataStoreClient(client);
+        algo.setFileResolver(fileResolver);
+        algo.setExecution(execution);
+        algo.run();
+
+        for(String s : execution.getStudyContexts()) {
+            System.out.println("found: " +client.get(StudyContext.class, s));
+            System.out.println("term: " +client.get(StudyContext.class, s).getTerm());
+            System.out.println("pattern: " +client.get(StudyContext.class, s).getPattern());
+        }
     }
-    
+
 }

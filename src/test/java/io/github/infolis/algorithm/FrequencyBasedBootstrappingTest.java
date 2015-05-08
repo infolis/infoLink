c1d80eb9c146536a7caca843e5a411f861e0e911
bolandka <katarina.boland@gmail.com>
2015-05-08 18:03:16 +0200

    tests for Learner / Frequency- and Reliability-based bootstrapping

--- src/test/java/io/github/infolis/algorithm/FrequencyBasedBootstrappingTest.java
+++ src/test/java/io/github/infolis/algorithm/FrequencyBasedBootstrappingTest.java
@@ -5,56 +5,85 @@
  */
 package io.github.infolis.algorithm;
 
+import static org.junit.Assert.assertEquals;
 import io.github.infolis.infolink.luceneIndexing.InfolisBaseTest;
 import io.github.infolis.model.Execution;
 import io.github.infolis.model.InfolisFile;
 import io.github.infolis.model.StudyContext;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  *
+ * @author kata
  * @author domi
  */
 public class FrequencyBasedBootstrappingTest extends InfolisBaseTest {
 
     Logger log = LoggerFactory.getLogger(SearchTermPositionTest.class);
 	private List<String> uris = new ArrayList<>();
-    private final static List<String> terms = Arrays.asList("FOOBAR");
-    
+	private final static String term = "FOOBAR";
+    private final static List<String> terms = Arrays.asList(term);
+    private static int termCount = 0;
     public FrequencyBasedBootstrappingTest() throws Exception {
 		for (InfolisFile file : createTestFiles()) {
             uris.add(file.getUri());
+            String str = FileUtils.readFileToString(new File(file.getFileName()));
+            termCount += StringUtils.countMatches(str, term);
 		}
 	}
-
-    @Test
-    public void testBootstrapping() throws Exception {
-
-        Execution execution = new Execution();
+    
+    
+    /**
+     * Tests basic functionality using no threshold for pattern induction (= accept all). 
+     * For a more detailed test refer to patternLearner.LearnerTest class.
+     * 
+     * @param strategy	
+     * @throws Exception
+     */
+    void testFrequencyBasedBootstrapping(Execution.Strategy strategy) throws Exception {
+    	
+    	Execution execution = new Execution();
         execution.setAlgorithm(FrequencyBasedBootstrapping.class);
         execution.getTerms().addAll(terms);
         execution.setInputFiles(uris);
         execution.setSearchTerm(terms.get(0));
-	execution.setThreshold(0.0);
-
+        execution.setThreshold(0.0);
+        execution.setBootstrapStrategy(strategy);
         Algorithm algo = new FrequencyBasedBootstrapping();
         algo.setDataStoreClient(localClient);
         algo.setFileResolver(tempFileResolver);
         algo.setExecution(execution);
         algo.run();
-
+        
+        int termCount_algo = 0;
         for(String s : execution.getStudyContexts()) {
 			System.out.println("found: " + localClient.get(StudyContext.class, s));
-			System.out.println("term: " + localClient.get(StudyContext.class, s).getTerm());
-			System.out.println("pattern: " + localClient.get(StudyContext.class, s).getPattern());
+			if(localClient.get(StudyContext.class, s).getTerm().equals(term)) termCount_algo++;
+			System.out.println("pattern: " + localClient.get(StudyContext.class, s).getPattern().getPatternRegex());
         }
+        // find all contexts for seed term in the first iteration
+        assertEquals(termCount, termCount_algo);
+        // also find all contexts for remaining terms using the pattern generated in iteration 1
+        assertEquals(uris.size(), execution.getStudyContexts().size());
+    }
+    
+    @Test
+    public void testBootstrapping_basic() throws Exception {
+
+    	testFrequencyBasedBootstrapping(Execution.Strategy.separate);
+    	testFrequencyBasedBootstrapping(Execution.Strategy.mergeCurrent);
+    	testFrequencyBasedBootstrapping(Execution.Strategy.mergeNew);
+    	testFrequencyBasedBootstrapping(Execution.Strategy.mergeAll);
     }
 
 }
--- src/test/java/io/github/infolis/infolink/patternLearner/LearnerTest.java
+++ src/test/java/io/github/infolis/infolink/patternLearner/LearnerTest.java
@@ -1,28 +1,122 @@
 package io.github.infolis.infolink.patternLearner;
 
-import io.github.infolis.infolink.patternLearner.Learner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import io.github.infolis.algorithm.Algorithm;
+import io.github.infolis.algorithm.FrequencyBasedBootstrapping;
+import io.github.infolis.algorithm.ReliabilityBasedBootstrapping;
+import io.github.infolis.algorithm.SearchTermPositionTest;
+import io.github.infolis.infolink.luceneIndexing.InfolisBaseTest;
 import io.github.infolis.model.Execution;
+import io.github.infolis.model.InfolisFile;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
-
-public class LearnerTest {
+/**
+*
+* @author kata
+*/
+public class LearnerTest extends InfolisBaseTest {
 	
-	// TODO: Do actual tests
-	@Test
-	public void test() {
-		assertThat(true, is(true));
-		boolean constraint_upperCase = false;
-		Learner l = new Learner(constraint_upperCase, "corpusPath", "trainPath", "contextPath", "arffPath", "outputPath", Execution.Strategy.separate);
-		assertThat(l, is(not(nullValue())));
-	}
+	Logger log = LoggerFactory.getLogger(SearchTermPositionTest.class);
+	private List<String> uris = new ArrayList<>();
+	private final static String term = "FOOBAR";
+    private final static List<String> terms = Arrays.asList(term);
 
-	@Test
-	public void testLearner() throws Exception {
-		throw new RuntimeException("not yet implemented");
+    public LearnerTest() throws Exception {
+    	
+		for (InfolisFile file : createTestFiles()) {
+            uris.add(file.getUri());
+		}
 	}
+	
+void testBootstrapping(Class<? extends Algorithm> algorithm, Execution.Strategy strategy, double threshold, Set<String> expectedStudies, Set<String> expectedPatterns, Set<String> expectedContexts) throws Exception {
+    	
+    	Execution execution = new Execution();
+        execution.setAlgorithm(algorithm);
+        execution.getTerms().addAll(terms);
+        execution.setInputFiles(uris);
+        execution.setSearchTerm(terms.get(0));
+        execution.setThreshold(threshold);
+        execution.setBootstrapStrategy(strategy);
+        Algorithm algo = algorithm.newInstance();
+        algo.setDataStoreClient(localClient);
+        algo.setFileResolver(tempFileResolver);
+        algo.setExecution(execution);
+        algo.run();
+        assertEquals(expectedStudies, new HashSet<String>(execution.getStudies()));
+        assertEquals(expectedPatterns, new HashSet<String>(execution.getPattern()));
+        assertEquals(expectedContexts, new HashSet<String>(execution.getStudyContexts()));
+    }
+    
+    private class ExpectedOutput {
+    	
+    	Class<? extends Algorithm> algorithm;
+    	Execution.Strategy strategy;
+    	double threshold;
+    	Set<String> studies;
+    	Set<String> patterns;
+    	Set<String> contexts;
+    	
+    	ExpectedOutput(Class<? extends Algorithm> algorithm, Execution.Strategy strategy, double threshold, Set<String> studies, Set<String> patterns, Set<String> contexts) {
+    		
+    		this.algorithm = algorithm;
+    		this.strategy = strategy;
+    		this.threshold = threshold;
+    		this.studies = studies;
+    		this.patterns = patterns;
+    		this.contexts = contexts;
+    	}
+    }
+    
+    //TODO: add actual values
+    Set<ExpectedOutput> getExpectedOutput() {
+    	
+    	Set<String> expectedStudies_separate = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedPatterns_separate = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedContexts_separate = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedStudies_mergeCurrent = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedPatterns_mergeCurrent = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedContexts_mergeCurrent = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedStudies_mergeNew = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedPatterns_mergeNew = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedContexts_mergeNew = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedStudies_mergeAll = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedPatterns_mergeAll = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedContexts_mergeAll = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedStudies_reliability = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedPatterns_reliability = new HashSet<String>(Arrays.asList("dummy"));
+    	Set<String> expectedContexts_reliability = new HashSet<String>(Arrays.asList("dummy"));
+    	
+    	Set<ExpectedOutput> expectedOutput = new HashSet<ExpectedOutput>();
+    	expectedOutput.addAll(Arrays.asList(
+    			new ExpectedOutput(FrequencyBasedBootstrapping.class, Execution.Strategy.separate, 0.2, expectedStudies_separate, expectedPatterns_separate, expectedContexts_separate),
+    			new ExpectedOutput(FrequencyBasedBootstrapping.class, Execution.Strategy.mergeCurrent, 0.2, expectedStudies_mergeCurrent, expectedPatterns_mergeCurrent, expectedContexts_mergeCurrent),
+    			new ExpectedOutput(FrequencyBasedBootstrapping.class, Execution.Strategy.mergeNew, 0.2, expectedStudies_mergeNew, expectedPatterns_mergeNew, expectedContexts_mergeNew),
+    			new ExpectedOutput(FrequencyBasedBootstrapping.class, Execution.Strategy.mergeAll, 0.2, expectedStudies_mergeAll, expectedPatterns_mergeAll, expectedContexts_mergeAll),
+    			new ExpectedOutput(ReliabilityBasedBootstrapping.class, Execution.Strategy.reliability, 0.4, expectedStudies_reliability, expectedPatterns_reliability, expectedContexts_reliability)
+    	));
+    	return expectedOutput;
+    }
+    
+    @Test
+    public void testBootstrapping() throws Exception {
+
+    	throw new RuntimeException ("Values for comparison missing");
+    	/*
+    	Set<ExpectedOutput> expectedOutputs = getExpectedOutput();
+    	for(ExpectedOutput expected : expectedOutputs) {
+    		testBootstrapping(expected.algorithm, expected.strategy, expected.threshold, expected.studies, expected.patterns, expected.contexts);
+    	}
+    	*/
+    }
 
 }

66af844660bf0d705c9472641c1a501eef6e21c4
bolandka <katarina.boland@gmail.com>
2015-05-09 02:14:52 +0200

    completed test

--- src/test/java/io/github/infolis/infolink/luceneIndexing/IndexerTest.java
+++ src/test/java/io/github/infolis/infolink/luceneIndexing/IndexerTest.java
@@ -4,15 +4,19 @@ import io.github.infolis.algorithm.Algorithm;
 import io.github.infolis.model.Execution;
 import io.github.infolis.model.InfolisFile;
 
-import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class IndexerTest extends InfolisBaseTest {
 
@@ -20,8 +24,9 @@ public class IndexerTest extends InfolisBaseTest {
 
 	@Test
 	public void testIndexing() throws Exception {
-		String indexDir = Files.createTempDirectory("infolis-test-").toString();
-
+		Path tempPath = Files.createTempDirectory("infolis-test-");
+		FileUtils.forceDeleteOnExit(tempPath.toFile());
+		
 		List<InfolisFile> inputFiles = createTestFiles();
 
 		List<String> uris = new ArrayList<>();
@@ -32,17 +37,18 @@ public class IndexerTest extends InfolisBaseTest {
 		Execution execution = new Execution();
 		execution.setAlgorithm(Indexer.class);
 		execution.setInputFiles(uris);
-		execution.setIndexDirectory(indexDir);
+		execution.setIndexDirectory(tempPath.toString());
 		
 		Algorithm algo = new Indexer();
 		algo.setExecution(execution);
 		algo.setFileResolver(tempFileResolver);
 		algo.setDataStoreClient(localClient);
 		algo.run();
-		
-//		log.debug("File 10: {} " , inputFiles.get(10));
-//		log.debug("File 10 Contents: {}", IOUtils.toString(fileResolver.openInputStream(inputFiles.get(10))));
-		FileUtils.deleteDirectory(new File(indexDir));
+
+		log.debug("File 0: {} " , inputFiles.get(0));
+		assertEquals("Hallo, please try to find the FOOBAR in this short text snippet. Thank you.", IOUtils.toString(tempFileResolver.openInputStream(inputFiles.get(0))));
+		log.debug("File 10: {} " , inputFiles.get(10));
+		assertEquals("Hallo, please try to find the term in this short text snippet. Thank you.", IOUtils.toString(tempFileResolver.openInputStream(inputFiles.get(10))));
 	}
 
 }

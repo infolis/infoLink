d507fafa19bf41a7e0d4fe22e047a95de9cbb29a
bolandka <katarina.boland@gmail.com>
2015-08-30 22:59:29 +0200

    added association to StudyContext URIs

--- src/main/java/io/github/infolis/model/InfolisPattern.java
+++ src/main/java/io/github/infolis/model/InfolisPattern.java
@@ -14,7 +14,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;

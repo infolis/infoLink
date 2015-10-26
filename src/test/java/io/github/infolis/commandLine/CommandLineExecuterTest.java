5e509a8e3d425fbd0e3490134529ebec024d2904
Konstantin Baierer <konstantin.baierer@bib.uni-mannheim.de>
2015-10-26 14:38:33 +0100

    io.File -> nio.Path, toUri: Make CommandLineExecuter work

--- src/test/java/io/github/infolis/commandLine/CommandLineExecuterTest.java
+++ src/test/java/io/github/infolis/commandLine/CommandLineExecuterTest.java
@@ -5,6 +5,7 @@ import io.github.infolis.InfolisBaseTest;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import java.util.UUID;
 
@@ -22,12 +23,12 @@ public class CommandLineExecuterTest extends InfolisBaseTest {
 	private static final Logger log = LoggerFactory.getLogger(CommandLineExecuterTest.class);
     
     //TODO: paths in the JSON are absolute like the inputFiles
-    @Ignore
+//    @Ignore
     @Test
-    public void test() throws FileNotFoundException, ClassNotFoundException, NoSuchFieldException, IllegalAccessException, IOException {
+    public void test() throws FileNotFoundException, ClassNotFoundException, NoSuchFieldException, IllegalAccessException, IOException, URISyntaxException {
     	String tempdir = "/tmp/infolis-test-" + UUID.randomUUID();
     	CommandLineExecuter.parseJson(
-    			Paths.get(getClass().getResource("/commandLine/algoDesc.json").getFile()),
+    			Paths.get(getClass().getResource("/commandLine/algoDesc.json").toURI()),
     			Paths.get(tempdir));
     	log.debug("Dumped execution to {}", tempdir);
     }

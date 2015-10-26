72148d5ad4a14eb5c02bb89a2e0a0b7f65f78796
dritze <dominique@informatik.uni-mannheim.de>
2015-10-26 14:05:22 +0100

    Enums are now supported by the CommandLineExecuter

--- src/main/java/io/github/infolis/commandLine/CommandLineExecuter.java
+++ src/main/java/io/github/infolis/commandLine/CommandLineExecuter.java
@@ -7,7 +7,9 @@ import io.github.infolis.datastore.DataStoreClientFactory;
 import io.github.infolis.datastore.DataStoreStrategy;
 import io.github.infolis.datastore.FileResolver;
 import io.github.infolis.datastore.FileResolverFactory;
+import io.github.infolis.model.BootstrapStrategy;
 import io.github.infolis.model.Execution;
+import io.github.infolis.model.MetaDataExtractingStrategy;
 import io.github.infolis.model.entity.InfolisFile;
 import io.github.infolis.util.SerializationUtils;
 
@@ -39,7 +41,7 @@ import org.apache.commons.io.IOUtils;
  * @author domi
  */
 public class CommandLineExecuter {
-    
+
     static protected DataStoreClient dataStoreClient;
     static protected FileResolver fileResolver;
 
@@ -47,18 +49,23 @@ public class CommandLineExecuter {
         JsonReader reader = Json.createReader(Files.newBufferedReader(jsonPath, Charset.forName("UTF-8")));
         JsonObject o = reader.readObject();
 
+        String wrongParameter = checkJsonFile(o);
+        if(!wrongParameter.isEmpty()) {
+            System.out.println(String.format("ERROR: %s is no parameter name", wrongParameter));
+        }
+
         Execution e = new Execution();
         //TODO: any better solution? e.g. to indicate the strategy as method parameter?
         dataStoreClient = DataStoreClientFactory.create(DataStoreStrategy.TEMPORARY);
         fileResolver = FileResolverFactory.create(DataStoreStrategy.TEMPORARY);
-        
+
         //iterate through the entries in the JSON file
         for (Entry<String, JsonValue> values : o.entrySet()) {
             switch (values.getValue().getValueType()) {
                 case STRING:
                 case NUMBER:
                 case TRUE:
-                case FALSE:    
+                case FALSE:
                     //algorithm has to be handled as a special case since we need to find the class
                     if (values.getKey().equals("algorithm")) {
                         String algorithmName = values.getValue().toString();
@@ -70,14 +77,24 @@ public class CommandLineExecuter {
                         e.setAlgorithm(algoClass);
                         break;
                     }
+                    if (values.getKey().equals("bootstrapStrategy")) {
+                        BootstrapStrategy b = BootstrapStrategy.valueOf(values.getValue().toString());
+                        e.setBootstrapStrategy(b);
+                        break;
+                    }
+                    if (values.getKey().equals("metaDataExtractingStrategy")) {
+                        MetaDataExtractingStrategy mde = MetaDataExtractingStrategy.valueOf(values.getValue().toString());
+                        e.setMetaDataExtractingStrategy(mde);
+                        break;
+                    }
                     //inputFiles need to be handled as a special case since we need to create the 
                     //files first and post them and convert them if necessary
-                    if(values.getKey().equals("inputFiles")) {
+                    if (values.getKey().equals("inputFiles")) {
                         String dir = values.getValue().toString().replace("\"", "");
-                        List<String> fileUris = postFiles(dir,dataStoreClient,fileResolver);                        
+                        List<String> fileUris = postFiles(dir, dataStoreClient, fileResolver);
                         e.setInputFiles(convertPDF(fileUris));
                         break;
-                    }                    
+                    }
                     //all other fields are just set
                     e.setProperty(values.getKey(), values.getValue().toString().replace("\"", ""));
                     break;
@@ -86,21 +103,40 @@ public class CommandLineExecuter {
                     JsonArray array = (JsonArray) values.getValue();
                     List<String> listEntries = new ArrayList<>();
                     for (int i = 0; i < array.size(); i++) {
-                        JsonString stringSeed = array.getJsonString(i);                        
+                        JsonString stringSeed = array.getJsonString(i);
                         listEntries.add(stringSeed.getString());
                     }
                     e.setProperty(values.getKey(), listEntries);
                     break;
-			default:
-				System.err.println("WARNING: Unhandled value type " + values.getValue().getValueType());
-				break;
+                default:
+                    System.err.println("WARNING: Unhandled value type " + values.getValue().getValueType());
+                    break;
             }
-        }       
-        dataStoreClient.post(Execution.class, e);        
+        }
+        dataStoreClient.post(Execution.class, e);
         e.instantiateAlgorithm(dataStoreClient, fileResolver).run();
         dataStoreClient.dump(outputDir);
     }
-    
+
+    /**
+     * checks whether all the variables in the Json file
+     * are indeed variables which can be set in the execution.
+     * 
+     * @param o
+     * @return 
+     */
+    private static String checkJsonFile(JsonObject o) {
+        for (Entry<String, JsonValue> values : o.entrySet()) {
+            try {
+                Execution e = new Execution();
+                e.getClass().getDeclaredField(values.getKey());
+            }catch(NoSuchFieldException ex) {
+                return values.getKey();
+            }
+        }
+        return "";
+    }
+
     private static List<String> convertPDF(List<String> uris) {
         List<String> txtUris = new ArrayList<>();
         for (String inputFileURI : uris) {
@@ -119,14 +155,13 @@ public class CommandLineExecuter {
                     convertExec.setAlgorithm(TextExtractorAlgorithm.class);
                     convertExec.setInputFiles(Arrays.asList(inputFile.getUri()));
                     // TODO wire this more efficiently so files are stored temporarily
-                    Algorithm algo = convertExec.instantiateAlgorithm(dataStoreClient,fileResolver);
+                    Algorithm algo = convertExec.instantiateAlgorithm(dataStoreClient, fileResolver);
                     algo.run();
                     // Set the inputFile to the file we just created
                     InfolisFile convertedInputFile = dataStoreClient.get(InfolisFile.class, convertExec.getOutputFiles().get(0));
                     txtUris.add(convertedInputFile.getUri());
-                } 
-            }
-            else {
+                }
+            } else {
                 txtUris.add(inputFileURI);
             }
         }
@@ -137,7 +172,7 @@ public class CommandLineExecuter {
         List<String> uris = new ArrayList<>();
         File dirFile = new File(dir);
         for (File f : dirFile.listFiles()) {
-            
+
             Path tempFile = Files.createTempFile("infolis-", ".pdf");
             InfolisFile inFile = new InfolisFile();
 
@@ -161,36 +196,39 @@ public class CommandLineExecuter {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            dsc.post(InfolisFile.class, inFile);       
+            dsc.post(InfolisFile.class, inFile);
             uris.add(inFile.getUri());
             inputStream.close();
         }
         return uris;
     }
-    
+
     public static void usage(String problem) {
-    	System.out.println(String.format("%s <json-path> <output-dir>", CommandLineExecuter.class.getSimpleName()));
-    	if (null != problem)
-    		System.out.println(String.format("ERROR: %s", problem));
-    	System.exit(1);
+        System.out.println(String.format("%s <json-path> <output-dir>", CommandLineExecuter.class.getSimpleName()));
+        if (null != problem) {
+            System.out.println(String.format("ERROR: %s", problem));
+        }
+        System.exit(1);
     }
-    
+
     public static void main(String args[]) throws FileNotFoundException, ClassNotFoundException, NoSuchFieldException, IllegalAccessException, IOException {
-    	if (args.length < 2)
-    		usage("Not enough arguments");
+        if (args.length < 2) {
+            usage("Not enough arguments");
+        }
 
-    	Path jsonPath = Paths.get(args[0]);
-    	if (! Files.exists(jsonPath) )
-    		usage("JSON doesn't exist");
+        Path jsonPath = Paths.get(args[0]);
+        if (!Files.exists(jsonPath)) {
+            usage("JSON doesn't exist");
+        }
 
-    	Path outputDir = Paths.get(args[1]);
-    	if (Files.exists(outputDir)) {
-    		System.err.println("WARNING: Output directory already exists, make sure it is empty.\nPress enter to continue or CTRL-C to exit");
-    		System.in.read();
-    	} else {
-    		Files.createDirectories(outputDir);
-    	}
+        Path outputDir = Paths.get(args[1]);
+        if (Files.exists(outputDir)) {
+            System.err.println("WARNING: Output directory already exists, make sure it is empty.\nPress enter to continue or CTRL-C to exit");
+            System.in.read();
+        } else {
+            Files.createDirectories(outputDir);
+        }
 
-		parseJson(jsonPath, outputDir);
+        parseJson(jsonPath, outputDir);
     }
 }

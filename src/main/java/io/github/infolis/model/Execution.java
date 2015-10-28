dcec727ac7200ca84afd1773697e70761afa7cce
Konstantin Baierer <konstantin.baierer@gmail.com>
2015-10-28 13:58:02 +0100

    Execution#setProperty: getClass() -> this

--- src/main/java/io/github/infolis/model/Execution.java
+++ src/main/java/io/github/infolis/model/Execution.java
@@ -528,14 +528,14 @@ public class Execution extends BaseModel {
     public void setProperty(String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
         Field field = this.getClass().getDeclaredField(fieldName);
         field.setAccessible(true);        
-        if (field.getType() == Character.TYPE) {field.set(getClass(), value.toString().charAt(0)); return;}
-        if (field.getType() == Short.TYPE) {field.set(getClass(), Short.parseShort(value.toString())); return;}
-        if (field.getType() == Integer.TYPE) {field.set(getClass(), Integer.parseInt(value.toString())); return;}
-        if (field.getType() == Long.TYPE) {field.set(getClass(), Long.parseLong(value.toString())); return;}
-        if (field.getType() == Float.TYPE) {field.set(getClass(), Float.parseFloat(value.toString())); return;}
-        if (field.getType() == Double.TYPE) {field.set(getClass(), Double.parseDouble(value.toString())); return;}
-        if (field.getType() == Byte.TYPE) {field.set(getClass(), Byte.parseByte(value.toString())); return;}
-        if (field.getType() == Boolean.TYPE) {field.set(getClass(), Boolean.parseBoolean(value.toString())); return;}
+        if (field.getType() == Character.TYPE) {field.set(this, value.toString().charAt(0)); return;}
+        if (field.getType() == Short.TYPE) {field.set(this, Short.parseShort(value.toString())); return;}
+        if (field.getType() == Integer.TYPE) {field.set(this, Integer.parseInt(value.toString())); return;}
+        if (field.getType() == Long.TYPE) {field.set(this, Long.parseLong(value.toString())); return;}
+        if (field.getType() == Float.TYPE) {field.set(this, Float.parseFloat(value.toString())); return;}
+        if (field.getType() == Double.TYPE) {field.set(this, Double.parseDouble(value.toString())); return;}
+        if (field.getType() == Byte.TYPE) {field.set(this, Byte.parseByte(value.toString())); return;}
+        if (field.getType() == Boolean.TYPE) {field.set(this, Boolean.parseBoolean(value.toString())); return;}
        // if (field.getGenericType() == );
         field.set(this, value);
     }

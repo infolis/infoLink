eddb5d1a75f03c89a78610ccb2ab09af221075d0
dritze <dominique@informatik.uni-mannheim.de>
2015-10-28 16:08:53 +0100

    Fixed the reflections and os-specific problems

--- src/test/java/io/github/infolis/commandLine/CommandLineExecuterTest.java
+++ src/test/java/io/github/infolis/commandLine/CommandLineExecuterTest.java
@@ -3,10 +3,8 @@ package io.github.infolis.commandLine;
 
 import io.github.infolis.InfolisBaseTest;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,12 +12,9 @@ import java.nio.file.Paths;
 import java.util.UUID;
 
 import org.apache.commons.io.IOUtils;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import sun.nio.ch.IOUtil;
 
 /**
  *
@@ -37,6 +32,26 @@ public class CommandLineExecuterTest extends InfolisBaseTest {
     	Path pdfDir = Paths.get(getClass().getResource("/examples/pdfs").toURI());
 
     	Path jsonin = Paths.get(getClass().getResource("/commandLine/algoDesc.json").toURI());
+    	Path tempjson = Paths.get(System.getProperty("java.io.tmpdir")+"/infolis-test-" + UUID.randomUUID() + ".json");
+
+    	String jsonString = IOUtils.toString(Files.newInputStream(jsonin));
+    	jsonString = jsonString.replace("INPUT_FILES_PATH", pdfDir.toString());
+        if(System.getProperty("os.name").contains("Windows")) {
+            jsonString = jsonString.replace("\\","\\\\");
+        }
+        
+    	IOUtils.write(jsonString, Files.newOutputStream(tempjson));
+    	log.debug(jsonString);
+    	CommandLineExecuter.parseJson(tempjson, tempdir);
+    	log.debug("Dumped execution to {}", tempdir);
+    }
+    
+    @Test
+    public void testDouble() throws FileNotFoundException, ClassNotFoundException, NoSuchFieldException, IllegalAccessException, IOException, URISyntaxException {
+    	Path tempdir = Files.createTempDirectory("infolis-test-" + UUID.randomUUID());
+    	Path pdfDir = Paths.get(getClass().getResource("/examples/pdfs").toURI());
+
+    	Path jsonin = Paths.get(getClass().getResource("/commandLine/double.json").toURI());
     	Path tempjson = Paths.get(System.getProperty("java.io.tmpdir")+"/infolis-test-" + UUID.randomUUID() + ".json");
 
     	String jsonString = IOUtils.toString(Files.newInputStream(jsonin));

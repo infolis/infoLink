5706ca5d5821ef82dcca45ddf60c91be3d5b8122
bolandka <katarina.boland@gesis.org>
2016-08-29 15:17:51 +0200

    link to entities in ontology

---
+++ src/main/java/io/github/infolis/algorithm/OntologyLinker.java
@@ -0,0 +1,95 @@
+package io.github.infolis.algorithm;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+import io.github.infolis.datastore.DataStoreClient;
+import io.github.infolis.datastore.FileResolver;
+import io.github.infolis.model.entity.Entity;
+import io.github.infolis.model.entity.EntityLink;
+
+/**
+ * 
+ * @author kata
+ * 
+ * Create links to entities in ontology, if study is included there.
+ *
+ */
+public class OntologyLinker extends MultiMatchesLinker {
+	
+	private static final Logger log = LoggerFactory.getLogger(OntologyLinker.class);
+	
+	public OntologyLinker(DataStoreClient inputDataStoreClient, DataStoreClient outputDataStoreClient, FileResolver inputFileResolver, FileResolver outputFileResolver) {
+		super(inputDataStoreClient, outputDataStoreClient, inputFileResolver, outputFileResolver);
+	}
+	
+	private String searchOntologyEntity(Entity entity, boolean onlyOntologyEntities) {
+		Multimap<String, String> query = HashMultimap.create();
+		// entities in ontology and used repositories have exactly one identifier
+		query.put("identifier", entity.getIdentifiers().get(0));
+		query.put("tags", "infolis-ontology");
+		List<Entity> entitiesInDatabase = getOutputDataStoreClient().search(Entity.class, query);
+		for (Entity entityInDatabase : entitiesInDatabase) {
+			debug(log, "Found entity in ontology: " + entityInDatabase.getUri());
+			return entityInDatabase.getUri();
+		}
+		debug(log, "Did not find entity in ontology");
+		if (onlyOntologyEntities) return null;
+		else return entity.getUri();
+	}
+	
+	/**
+	 * Return ontology entry for entity if existing.
+	 * If not, 
+	 * <ul>
+	 * <li>return null if onlyOntologyEntities is set</li>
+	 * <li>return URI of entity if onlyOntologyEntities is not set</li>
+	 * </ul>
+	 * @param entity
+	 * @param onlyOntologyEntities
+	 * @return
+	 */
+	private String getOntologyEntity(Entity entity, boolean onlyOntologyEntities) {
+		String ontologyUri = "dataset_" + entity.getIdentifiers().get(0)
+				.replace("/", "")
+				.replace(".", "");
+		Entity ontologyEntity = getOutputDataStoreClient().get(Entity.class, ontologyUri);
+		if (null != ontologyEntity) {
+			return ontologyUri;
+		} else {
+			if (onlyOntologyEntities) return null;
+			else return entity.getUri();
+		}
+	}
+	
+	protected List<String> refineLinksUsingOntology(List<String> links) {
+		for (EntityLink link : getOutputDataStoreClient().get(EntityLink.class, links)) {
+			Entity toEntity = getOutputDataStoreClient().get(Entity.class, link.getToEntity());
+			String entityUri = getOntologyEntity(toEntity, false);
+			if (null == entityUri) {
+				//TODO implement delete method and use here
+				//getOutputDataStoreClient().delete(link.getUri());
+				//getOutputDataStoreClient().delete(toEntity.getUri());
+				links.remove(link.getUri());
+			}
+			else {
+				EntityLink newLink = new EntityLink(link.getFromEntity(), entityUri, link.getConfidence(), 
+						link.getLinkReason(), link.getEntityRelations());
+				getOutputDataStoreClient().put(EntityLink.class, newLink, link.getUri());
+			}
+		}
+		return links;
+	}
+	
+	@Override
+	public void execute() {
+		super.execute();
+		List<String> links = getExecution().getLinks();
+		getExecution().setLinks(refineLinksUsingOntology(links));
+	}
+}
---
+++ src/test/java/io/github/infolis/algorithm/OntologyLinkerTest.java
@@ -0,0 +1,80 @@
+package io.github.infolis.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.github.infolis.InfolisBaseTest;
+import io.github.infolis.model.Execution;
+import io.github.infolis.model.entity.Entity;
+import io.github.infolis.model.entity.EntityLink;
+import io.github.infolis.model.entity.EntityLink.EntityRelation;
+
+/**
+ * 
+ * @author kata
+ *
+ */
+public class OntologyLinkerTest extends InfolisBaseTest {
+	
+	private static final Logger log = LoggerFactory.getLogger(OntologyLinkerTest.class);
+	
+	@Before
+	public void loadTestOntology() {
+		Entity dataset1 = new Entity();
+		Entity dataset2 = new Entity();
+		dataset1.addIdentifier("id1");
+		dataset2.addIdentifier("id2");
+		dataset1.addTag("infolis-ontology");
+		dataset2.addTag("infolis-ontology");
+		dataset1.setName("ontologyDataset1");
+		dataset1.setSpatial(new HashSet<>(Arrays.asList("spatial")));
+		dataStoreClient.put(Entity.class, dataset1, "dataset_id1");
+		dataStoreClient.put(Entity.class, dataset2, "dataset_id2");
+		EntityLink link = new EntityLink();
+		link.setFromEntity(dataset1.getUri());
+		link.setToEntity(dataset2.getUri());
+		link.addTag("infolis-ontology");
+		link.addEntityRelation(EntityRelation.part_of);
+		dataStoreClient.post(EntityLink.class, link);
+	}
+	
+	@Test
+	public void test() {
+		Entity fromEntity1 = new Entity();
+		Entity toEntity1 = new Entity();
+		toEntity1.addIdentifier("id1");
+		toEntity1.setName("dataset1");
+		dataStoreClient.post(Entity.class, fromEntity1);
+		dataStoreClient.post(Entity.class, toEntity1);
+		EntityLink link = new EntityLink();
+		link.setFromEntity(fromEntity1.getUri());
+		link.setToEntity(toEntity1.getUri());
+		link.addEntityRelation(EntityRelation.same_as_temporal);
+		dataStoreClient.post(EntityLink.class, link);
+		List<String> links = Arrays.asList(link.getUri());
+		
+		Execution exec = new Execution();
+		exec.setLinks(links);
+		OntologyLinker ontoLinker = new OntologyLinker(
+				dataStoreClient, dataStoreClient, fileResolver, fileResolver);
+		ontoLinker.setExecution(exec);
+		
+		for (EntityLink newLink : dataStoreClient.get(EntityLink.class, ontoLinker.refineLinksUsingOntology(links))) {
+			Entity toEntity = dataStoreClient.get(Entity.class, newLink.getToEntity());
+			log.debug("fromEntity: " + newLink.getFromEntity());
+			log.debug("toEntity: " + toEntity.getName());
+			log.debug("toEntitySpatial: " + toEntity.getSpatial());
+			log.debug("entityRelations: " + newLink.getEntityRelations());
+			
+			assertEquals("ontologyDataset1", toEntity.getName());
+		}
+	}
+}
